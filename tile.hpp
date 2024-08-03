f50e9b24e48bc8e997efe43197b41f37416a07b5
Kristian Garkov <kristian.garkov04@gmail.com>
2024-08-03 17:42:51 -0400

    Added default constructor and made overloaded operator a friend function

--- tile.hpp
+++ tile.hpp
@@ -6,11 +6,15 @@ class Tile {
 public:
     Tile(int num) : m_numOfBombs {num} {}
 
+    Tile() = default;
+
     int getNum() const { return m_numOfBombs; }
     void setNum(int num) { m_numOfBombs = num; }
     bool isBomb() const { return m_numOfBombs == -1; } // -1 will indicate that a tile contains a bomb.
     bool isEmpty() const { return m_numOfBombs == 0; }
 
+    friend std::ostream& operator<<(std::ostream& out, const Tile& t);
+
 private:
-    int m_numOfBombs;
+    int m_numOfBombs = 0;
 };
