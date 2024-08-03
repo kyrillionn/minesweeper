18046f9cfc71e07c19bdeb143ab3a478ce6eab7a
Kristian Garkov <kristian.garkov04@gmail.com>
2024-08-03 19:01:33 -0400

    Added getters for the fields

--- board.hpp
+++ board.hpp
@@ -1,18 +1,21 @@
 #pragma once
 
 #include "tile.hpp"
+#include "point.hpp"
 
 class Board {
 public:
     Board() { randomize(); }
 
     void randomize();
+    static int getBoardSize() { return m_boardSize; };
+    static int getMaxMines() { return m_maxMines; };
 
     friend std::ostream& operator<<(std::ostream& out, const Board& b);
 
 private:
     static const int m_boardSize = 8;
-    const int m_maxMines = 10;
+    static const int m_maxMines = 10;
 
     Tile m_board[m_boardSize][m_boardSize];
 };
