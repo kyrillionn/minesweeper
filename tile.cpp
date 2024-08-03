e96dcaf9dac53f8e749a2f8a96595eafaae69a98
Kristian Garkov <kristian.garkov04@gmail.com>
2024-08-03 17:50:55 -0400

    Added ability to hide tiles

--- tile.cpp
+++ tile.cpp
@@ -1,6 +1,9 @@
 #include "tile.hpp"
 
 std::ostream& operator<<(std::ostream& out, const Tile& t) {
+    if (t.isHidden())
+        return out << "X";
+
     if (t.isMine())
         return out << "*";
 
--- tile.hpp
+++ tile.hpp
@@ -12,9 +12,12 @@ public:
     void setNum(int num) { m_numOfMines = num; }
     bool isMine() const { return m_numOfMines == -1; } // -1 will indicate that a tile contains a mine.
     bool isEmpty() const { return m_numOfMines == 0; }
+    bool isHidden() const { return m_hidden; }
+    void setHidden(bool h) { m_hidden = h; }
 
     friend std::ostream& operator<<(std::ostream& out, const Tile& t);
 
 private:
     int m_numOfMines = 0;
+    bool m_hidden = true;
 };
