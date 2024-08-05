fdbac7d4915bfa11e8634a9b4702c9d74874a4c0
Kristian Garkov <kristian.garkov04@gmail.com>
2024-08-05 17:48:30 -0400

    Started making main game loop

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
 #include "board.hpp"
+#include "point.hpp"
 
 #include <iostream>
 
+Point getUserInput() {
+    // Doesn't do any validation because im lazy
+
+    std::cout << "Enter a point on the board (e.g. 2,3): ";
+    int x;
+    char ignore;
+    int y;
+
+    std::cin >> x >> ignore >> y;
+
+    return Point{x, y};
+}
+
 int main() {
+    std::cout << "Welcome to Minesweeper!\n\n";
+
     Board b;
 
-    std::cout << b;
+    while (true) {
+        std::cout << b << "\n\n";
+        Point p = getUserInput();
+
+        int tileNum = b.revealTiles(p);
+
+        if (tileNum == -1) {
+            std::cout << b << "\n You lost.";
+            break;
+        }
+    }
+
+    std::cout << "Thank you for playing!";
 
     return 0;
 }
